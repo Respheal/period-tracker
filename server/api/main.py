24952c24fcc9f04f4c74935d0a51fc5eea45f8a4
Respheal <madeleine.ethridge@gmail.com>
2025-12-09 22:55:14 -0500

    setting up pydantic-settings

--- server/api/main.py
+++ server/api/main.py
@@ -1,14 +1,17 @@
-from fastapi import FastAPI
+from typing import Annotated
 
-from .routers import items, users
+from fastapi import Depends, FastAPI
+
+from api.routers import items, users
+from api.utils.config import Settings
+from api.utils.dependencies import get_settings
 
 app = FastAPI()
-
 
 app.include_router(users.router)
 app.include_router(items.router)
 
 
 @app.get("/")
-async def root() -> dict[str, str]:
-    return {"message": "Health Check"}
+async def root(settings: Annotated[Settings, Depends(get_settings)]) -> dict[str, str]:
+    return {"message": settings.app_name}
--- server/api/utils/dependencies.py
+++ server/api/utils/dependencies.py
@@ -1,8 +1,15 @@
 from collections.abc import Generator
+from functools import lru_cache
 
 from sqlmodel import Session
 
 from api.db.session import engine
+from api.utils.config import Settings
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
 
 
 def get_session() -> Generator[Session]:
