5e7fb6e3b9d7cb39bf6911ae5719e74856da5fda
Respheal <madeleine.ethridge@gmail.com>
2026-01-18 14:07:29 -0500

    setting up a basic npm action

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "period-tracker",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
 });
