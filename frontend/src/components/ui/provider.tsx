28a144d2dd2fac707431daf8210d4bc396790fb4
Respheal <madeleine.ethridge@gmail.com>
2026-04-11 22:14:42 -0400

    adding a custom default button so I can see it better

--- frontend/src/components/ui/provider.tsx
+++ frontend/src/components/ui/provider.tsx
@@ -1,15 +1,40 @@
-"use client"
+'use client';
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
+  ChakraProvider,
+  createSystem,
+  defaultConfig,
+  defineRecipe,
+} from '@chakra-ui/react';
+import { ColorModeProvider, type ColorModeProviderProps } from './color-mode';
+
+const buttonRecipe = defineRecipe({
+  variants: {
+    variant: {
+      cyan: {
+        bg: 'bg',
+        borderColor: 'cyan.fg',
+        color: 'cyan.fg',
+        _hover: { bg: 'cyan.emphasized' },
+        _selected: { bg: 'cyan.fg', color: 'cyan.muted', _hover: { color: 'cyan.fg' } },
+      },
+    },
+  },
+  defaultVariants: {
+    variant: 'cyan',
+  },
+});
+
+const system = createSystem(defaultConfig, {
+  theme: {
+    recipes: { button: buttonRecipe },
+  },
+});
 
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={system}>
       <ColorModeProvider {...props} />
     </ChakraProvider>
-  )
+  );
 }
